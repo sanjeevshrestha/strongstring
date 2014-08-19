3295dfb4fc1f158a4c5ce4d394dfdbc0bb64bdf4
Sanjeev Shrestha <Sanjeevshrestha2004@gmail.com>
2014-08-19 13:40:18 +0545

    Changes

--- generate.php
+++ generate.php
@@ -5,7 +5,7 @@
 * @filename generate.php
 * date: 19 Aug 2014
 * Usage: php generate.php <length> <class>
-* e.g. php generate.php 10 1
+* e.g. php generate.php 10 0, php generate.php 15 2, php generate.php 12 1
 */
 //require the file
 require_once(dirname(__FILE__).'/strongstring.php');
